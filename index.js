7dbcd8bd132e386bbddb9ca71ba4a91febc39306
saniya <saniyagarg9@gmail.com>
2025-07-17 02:26:12 +0530

    added the  query paramater

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const http=require("http")
 const fs=require("fs")
+const url=require("url");
 const myServer=http.createServer((req,res)=>{
+    if(req.url==="/favicon.ico")
+    {
+        return res.end();
+    }
     const log=`${Date.now()}: ${req.url} New Request Recieved \n`
+    const myURL=url.parse(req.url,true);
+    console.log(myURL);
     fs.appendFile("log.txt",log,(err,data)=>
     {
-        switch(req.url)
+        switch(myURL.pathname)
         {
             case '/':res.end("Home Page");
             break;
-            case '/about': res.end("saniya garg")
+            case '/about':
+            {const username=myURL.query.name;
+            res.end(`Hi ${username}`);}
             break;
+            case '/search':
+              {  const search=myURL.query.search;
+                res.end(`Your  results are ${search}`)
+                break;}
             default:
                 res.end("Error 404");
 
